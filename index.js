e67c400fc653ca1ad2e6a9914d810748904e2d69
LorenzoCampanella98 <84874986+LorenzoCampanella98@users.noreply.github.com>
2023-10-23 14:40:52 +0000

    I upload the repository

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+// ------------------- USE express-useranget library ---------------------
+const useragent = require('express-useragent');
+app.use(useragent.express());
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -15,16 +20,29 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// ------------------------------- SOLUTION ------------------------------
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    //obtain the IP
+    'ipaddress': req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    //obtain the language
+    'language' : req.acceptsLanguages(),
+    //Use the library useragnet to get source
+    'software' : req.useragent.source
+  })
+})
